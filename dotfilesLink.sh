3e52c3a1ea25017cfb3ef325e87d6a736ba5a139
Yusuke Sonoda <y.snd12p@gmail.com>
2021-09-26 20:44:16 +0900

    .gitconfigを追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,6 +3,7 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zlogout ~/.zlogout
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 # mkdir -p ~/.vim/rc
 # ln -sf ~/dotfiles/dein.toml ~/.vim/rc
 # ln -sf ~/dotfiles/dein_lazy.toml ~/.vim/rc
