5aecbe2a423cedfdbb45b0ad4638d7b5cc6b78bb
Yusuke Sonoda <y.snd12p@gmail.com>
2021-10-18 22:33:52 +0900

    dotfilesLink.shのコミット忘れ

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zlogout ~/.zlogout
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 # mkdir -p ~/.vim/rc
