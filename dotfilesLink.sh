f3342d1da1337b53054f08c44c90b43fe49b1a1f
Yusuke Sonoda <y.snd12p@gmail.com>
2024-12-31 22:36:54 +0900

    シンボリックリンクの作成先に変数名を使用するように変更

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,16 +2,16 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 CFGDIR=${XDG_CONFIG_HOME:=$HOME/.config}
 
-mkdir -p ${CFGDIR}/nvim/lua/config ${CFGDIR}/dein ${CFGDIR}/coc
+mkdir -p ${CFGDIR}/nvim/lua/config ${CFGDIR}/sheldon ${CFGDIR}/dein ${CFGDIR}/coc
 ln -sf ${SCRIPT_DIR}/.vimrc ~/.vimrc
 ln -sf ${SCRIPT_DIR}/.zshrc ~/.zshrc
-ln -sf ${SCRIPT_DIR}/plugins.toml ${XDG_CONFIG_HOME}/sheldon/plugins.toml
+ln -sf ${SCRIPT_DIR}/plugins.toml ${CFGDIR}/sheldon/plugins.toml
 ln -sf ${SCRIPT_DIR}/.tmux.conf ~/.tmux.conf
 ln -sf ${SCRIPT_DIR}/.gitconfig ~/.gitconfig
 # ln -sf ${SCRIPT_DIR}/init.vim ~/.config/nvim
-ln -sf ${SCRIPT_DIR}/init.lua ~/.config/nvim
-ln -sf ${SCRIPT_DIR}/lazy.lua ~/.config/nvim/lua/config
-ln -sf ${SCRIPT_DIR}/dein.toml ~/.config/dein
-ln -sf ${SCRIPT_DIR}/dein_lazy.toml ~/.config/dein
-ln -sf ${SCRIPT_DIR}/coc-setting.vim ~/.config/coc
+ln -sf ${SCRIPT_DIR}/init.lua ${CFGDIR}/nvim
+ln -sf ${SCRIPT_DIR}/lazy.lua ${CFGDIR}/nvim/lua/config
+ln -sf ${SCRIPT_DIR}/dein.toml ${CFGDIR}/dein
+ln -sf ${SCRIPT_DIR}/dein_lazy.toml ${CFGDIR}/dein
+ln -sf ${SCRIPT_DIR}/coc-setting.vim ${CFGDIR}/coc
 ln -snf ${SCRIPT_DIR}/bin ~/bin
