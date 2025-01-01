f7e4549cad11aefb2accdcf9636cd4afcaf3c30c
Yusuke Sonoda <y.snd12p@gmail.com>
2025-01-01 11:35:41 +0900

    nvim-treeの設定変更

--- nvim_plugins/nvim-tree.lua
+++ nvim_plugins/nvim-tree.lua
@@ -1,5 +1,15 @@
 -- https://github.com/nvim-tree/nvim-tree.lua
 return {
   "nvim-tree/nvim-tree.lua",
+  -- https://github.com/nvim-tree/nvim-web-devicons
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = true,
+  keys = {
+    {mode = "n", "<leader>n", "<cmd>NvimTreeToggle<CR>", desc = "NvimTreeをトグル"},
+  },
+  opts = {
+    view = {
+      width = 40,
+    },
+  },
 }
