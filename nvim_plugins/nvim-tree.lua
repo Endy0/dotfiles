fcd0d64f5fbc485f87afb9c164c3599391f319a3
Yusuke Sonoda <y.snd12p@gmail.com>
2025-01-02 17:59:06 +0900

    tiny fix

--- nvim_plugins/nvim-tree.lua
+++ nvim_plugins/nvim-tree.lua
@@ -1,10 +1,10 @@
 -- https://github.com/nvim-tree/nvim-tree.lua
 return {
-  "nvim-tree/nvim-tree.lua",
+  'nvim-tree/nvim-tree.lua',
   -- https://github.com/nvim-tree/nvim-web-devicons
-  dependencies = {"nvim-tree/nvim-web-devicons"},
+  dependencies = {'nvim-tree/nvim-web-devicons'},
   keys = {
-    {mode = "n", "<leader>n", "<cmd>NvimTreeToggle<CR>", desc = "NvimTreeをトグル"},
+    {mode = 'n', '<leader>n', '<cmd>NvimTreeToggle<CR>', desc = 'NvimTreeをトグル'},
   },
   opts = {
     view = {
