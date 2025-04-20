5337fb0fae97867e9ae9858c23623b049db5cd9f
Yusuke Sonoda <y.snd12p@gmail.com>
2025-04-20 16:13:41 +0900

    lualine追加

---
+++ nvim_plugins/lualine.lua
@@ -0,0 +1,9 @@
+-- https://github.com/nvim-lualine/lualine.nvim
+return {
+  'nvim-lualine/lualine.nvim',
+  -- https://github.com/nvim-tree/nvim-web-devicons
+  dependencies = {'nvim-tree/nvim-web-devicons'},
+  opts = {
+    options = { theme = 'ayu_dark' }
+  }
+}
